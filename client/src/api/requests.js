e8086358b3d75eb2fd241d233fb2a1fe4f494287
11ac11 <alexander.crump11@gmail.com>
2024-09-02 22:17:12 +0200

    updated all functions with nocache

--- client/src/api/requests.js
+++ client/src/api/requests.js
@@ -9,11 +9,12 @@ const proxyUrl = 'https://corsproxy.io/?';
 // const proxyUrl = 'https://cors-buster.vercel.app/?href=';
 const targetUrl = 'https://fantasy.premierleague.com/api/';
 const encodedUrl = proxyUrl + targetUrl;
+const noCache = `?nocache=${Date.now()}`
 
 export const getAllGameweekInfo = async () => {
   try {
     // const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://fantasy.premierleague.com/api/bootstrap-static/')}`)
-    const response = await fetch(`${encodedUrl}bootstrap-static/?nocache=${Date.now()}`)
+    const response = await fetch(`${encodedUrl}bootstrap-static/${noCache}`)
     // headers: { 'x-cors-api-key': 'temp_d30690954ee0284086917763ef226e01' }
 
     if (!response.ok) {
@@ -46,7 +47,7 @@ export const getAllGameweekInfo = async () => {
 
 export const fetchLeagueStandings = async (leagueId, gameweekNumber) => {
   try {
-    const response = await fetch(`${encodedUrl}/leagues-h2h/${leagueId}/standings/?page_new_entries=1&page_standings=1`);
+    const response = await fetch(`${encodedUrl}/leagues-h2h/${leagueId}/standings/?page_new_entries=1&page_standings=1${noCache}`);
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status} `);
     }
@@ -62,7 +63,7 @@ export const fetchLeagueStandings = async (leagueId, gameweekNumber) => {
 
 export const fetchFantasyFixtures = async (leagueId, gameweek) => {
   try {
-    const response = await fetch(`${encodedUrl}/leagues-h2h-matches/league/${leagueId}/?page=1&event=${gameweek}`);
+    const response = await fetch(`${encodedUrl}/leagues-h2h-matches/league/${leagueId}/?page=1&event=${gameweek}${noCache}`);
 
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`);
@@ -79,7 +80,7 @@ export const fetchFantasyFixtures = async (leagueId, gameweek) => {
 
 export const fetchLivePlayerScores = async (gameweekNumber) => {
   try {
-    const response = await fetch(`${encodedUrl}event/${gameweekNumber}/live/`);
+    const response = await fetch(`${encodedUrl}event/${gameweekNumber}/live/${noCache}`);
 
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`);
@@ -96,7 +97,7 @@ export const fetchLivePlayerScores = async (gameweekNumber) => {
 export const fetchManagerPicksByEvent = async (managerId, gameweekNumber) => {
   try {
     const response = await fetch(
-      `${encodedUrl}team/${managerId}/event/${gameweekNumber}/picks/`
+      `${encodedUrl}team/${managerId}/event/${gameweekNumber}/picks/${noCache}`
     );
 
     if (!response.ok) {
@@ -114,7 +115,7 @@ export const fetchManagerPicksByEvent = async (managerId, gameweekNumber) => {
 export const fetchPremFixtures = async (gameweekNumber) => {
   try {
     const response = await fetch(
-      `${encodedUrl}/fixtures/?event=${gameweekNumber}`
+      `${encodedUrl}/fixtures/?event=${gameweekNumber}${noCache}`
     );
 
     if (!response.ok) {
